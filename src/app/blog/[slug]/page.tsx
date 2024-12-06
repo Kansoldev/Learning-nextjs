61b7c99a5afa867f6ab39c1d2acbdfb1aa8d9607
Oyinkansola Yahaya <yahayaoyinkansola@gmail.com>
2024-12-06 13:09:28 +0100

    Learnt how to create routes using the App router
    Routes are created in the app folder, each route is a folder with a file called page.jsx (or page.tsx) that contains the content of that page e.g if the route is /about, the folder name will be "about" and page.jsx will contain the content for that page
    
    I can also created nested routes by just following the same procedure and creating a folder inside another folder with it's own page.jsx file

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,5 @@
+const BlogArticle = () => {
+  return <div>Blog article</div>;
+};
+
+export default BlogArticle;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Start of project</h1>;
+  return <h1>Home page</h1>;
 }
