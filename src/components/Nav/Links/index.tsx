f12b998b972b1141c7df010c9446b495b5c454e3
Oyinkansola Yahaya <yahayaoyinkansola@gmail.com>
2024-12-26 13:13:25 +0100

    Make navbar responsive

--- src/components/Nav/Links/index.tsx
+++ src/components/Nav/Links/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 
 type linksType = { title: string; path: string }[];
@@ -25,24 +26,55 @@ const links: linksType = [
 ];
 
 const Links = () => {
+  const [open, setOpen] = useState(false);
   const pathname = usePathname();
 
   return (
-    <div className="flex items-center gap-6">
-      {links.map((link) => (
-        <Link
-          key={link.title}
-          href={link.path}
-          className={`text-base ${
-            pathname == link.path
-              ? "bg-white text-[#0d0c22] p-[10px] rounded-[20px] font-medium text-center"
-              : ""
-          }`}
+    <>
+      <div className="hidden md:flex items-center gap-6">
+        {links.map((link) => (
+          <Link
+            key={link.title}
+            href={link.path}
+            className={`text-base ${
+              pathname == link.path
+                ? "bg-white text-[#0d0c22] p-[10px] rounded-[20px] font-medium text-center"
+                : ""
+            }`}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </div>
+
+      <button
+        className="bg-white text-[#0d0c22] p-[10px] md:hidden"
+        onClick={() => setOpen(!open)}
+      >
+        Menu
+      </button>
+
+      {open && (
+        <div
+          className="absolute top-[100px] right-8 w-1/2 flex flex-col items-end gap-10 mt-20 md:hidden"
+          style={{ backgroundColor: "#0d0c22", height: "calc(100vh - 100px)" }}
         >
-          {link.title}
-        </Link>
-      ))}
-    </div>
+          {links.map((link) => (
+            <Link
+              key={link.title}
+              href={link.path}
+              className={`text-base ${
+                pathname == link.path
+                  ? "bg-white text-[#0d0c22] p-[10px] rounded-[20px] font-medium text-center"
+                  : ""
+              }`}
+            >
+              {link.title}
+            </Link>
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
