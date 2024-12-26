377a63eaade84430e996a298a81e3a27b6bd2110
Oyinkansola Yahaya <yahayaoyinkansola@gmail.com>
2024-12-26 12:02:27 +0100

    Add class for navbar

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -2,10 +2,10 @@ import Links from "./Links";
 
 const NavBar = () => {
   return (
-    <>
-      <div>Logo</div>
+    <div className="h-[100px] flex items-center justify-between">
+      <div className="text-3xl font-bold">Logo</div>
       <Links />
-    </>
+    </div>
   );
 };
 
